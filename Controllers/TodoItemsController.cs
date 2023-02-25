117ccdbbeaeb7263085f91b6ac5c49d8214ccf85
Chukwuebukaodunze <53659953+henryOd@users.noreply.github.com>
2023-02-25 10:52:22 +0100

    Add project files.

---
+++ Controllers/TodoItemsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoItemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItemsDTO>>> GetTodoItems()
+        {
+            return await _context.TodoItems
+                .Select(x => ItemsToDTO(x))
+                .ToListAsync();
+        }
+
+        // GET: api/TodoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItemsDTO>> GetTodoItem(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return ItemsToDTO(todoItem);
+        }
+
+        // PUT: api/TodoItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(int id, TodoItemsDTO todoDTO)
+        {
+            if (id != todoDTO.Id)
+            {
+                return BadRequest();
+            }
+
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+            todoItem.Name=todoDTO.Name;
+            todoItem.IsComplete = todoDTO.IsCompleted;
+
+            //_context.Entry(todoItem).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TodoItemsDTO>> PostTodoItem(TodoItemsDTO todoDTO)
+
+        {
+            var TodoItem = new TodoItem()
+            {
+                Name = todoDTO.Name,
+                IsComplete = todoDTO.IsCompleted,
+            };
+
+            _context.TodoItems.Add(TodoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = TodoItem.Id }, ItemsToDTO(TodoItem));
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoItemExists(int id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+
+        public static TodoItemsDTO ItemsToDTO(TodoItem todoitem) => new TodoItemsDTO()
+        {
+            Id=todoitem.Id,
+            Name=todoitem.Name,
+            IsCompleted=todoitem.IsComplete
+        };
+
+        
+
+
+    }
+}
---
+++ Models/TodoItemsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItemsDTO
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
